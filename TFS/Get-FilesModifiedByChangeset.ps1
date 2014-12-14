d85377967755d282c9da34b63ea141ab7a0cb595
Shane Carvalho <shanec_@hotmail.com>
2014-12-14 16:47:28 +0530

    Completed core functionality
    Completed the core functionality for the script

--- TFS/Get-FilesModifiedByChangeset.ps1
+++ TFS/Get-FilesModifiedByChangeset.ps1
@@ -1,14 +1,27 @@
+#================================================
+# Script to get a list of files changed since between two changesets or dates
+# Works only with Visual Studio Online
+# Author: Shane Carvalho (shanec_@hotmail.com)
+# Date: 2014/12/14
+#
+# Usage:
+#    .\Get-FilesModifiedByChangeset.ps1 -projectCollectionUrl "https://myaccount.visualstudio.com/defaultcollection/" -itemPath "$/sandbox" -startChangesetId "5"  -endChangesetId "6"
+#
+#================================================
+
 param(
-    [string]$projectUrl,
+    [string]$projectCollectionUrl,
+    [string]$itemPath,
     [string]$startChangesetId,
     [string]$endChangesetId,
     [string]$startDate,
     [string]$endDate
     )
 
-$projectUrl = "https://silfen.visualstudio.com/defaultcollection/"
-#$startChangesetId = "3232312"
-$endChangesetId = "6"
+#$projectCollectionUrl = "https://<account>.visualstudio.com/defaultcollection/"
+#$itemPath = "$/sandbox"
+#$startChangesetId = "5"
+#$endChangesetId = "6"
 
 $cred = Get-Credential
 
@@ -19,7 +32,8 @@ $password = $cred.GetNetworkCredential().Password;
 function Invoke-Service([string]$restOperation, [string]$restParams)
 {
 
-    #http://stuartpreston.net/2014/05/accessing-visual-studio-online-rest-api-using-powershell-4-0-invoke-restmethod-and-alternate-credentials/
+    #basic authentication work around
+	#http://stuartpreston.net/2014/05/accessing-visual-studio-online-rest-api-using-powershell-4-0-invoke-restmethod-and-alternate-credentials/
 
     #get the credentials
     $basicAuth = ("{0}:{1}" -f $username,$password)
@@ -34,18 +48,37 @@ function Invoke-Service([string]$restOperation, [string]$restParams)
     
     $requestUri = $baseUrl + $restOperation + "?api-version=1.0&" + $restParams
 
-    $request = Invoke-RestMethod -Uri $requestUri -Method Get -Headers $headers
+    $result = Invoke-RestMethod -Uri $requestUri -Method Get -Headers $headers
 
+	return $result
 }
 
-######### logic logic
-
-$changesetsOperation = 'changesets'
+### Generate the parameters and filters necessary
 $changesetsParams = ""
 
+if($itemPath) { $changesetsParams += "&searchCriteria.itemPath=$itemPath" }
 if($startChangesetId) { $changesetsParams += "&searchCriteria.fromId=$startChangesetId" }
 if($endChangesetId) { $changesetsParams += "&searchCriteria.toId=$endChangesetId" }
 if($startDate) { $changesetsParams += "&searchCriteria.fromDate=$startDate" }
 if($endDate) { $changesetsParams += "&searchCriteria.toDate=$endDate" }
 
-Invoke-Service $changesetsOperation $changesetsParams
+$changesetResults = Invoke-Service 'changesets' $changesetsParams
+
+#iterate through all the changesets to figure out the project we need
+foreach ($cid in $changesetResults.value.changesetId)
+{
+    $changesetDetail = Invoke-Service "changesets/$cid/changes" ""
+    $changes = $changesetDetail.value | ConvertTo-Json | ConvertFrom-Json
+
+    foreach($ch in $changes)
+    {
+        #ignore the folders
+        if($ch.item.isFolder -ne $true)
+        {
+            if($ch.item.path)
+            {
+                Write-Host $ch.item.path
+            }
+        }
+    }
+}
