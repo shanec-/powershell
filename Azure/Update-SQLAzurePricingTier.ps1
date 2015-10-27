628f2e2e8f692ac225e165650db1fe27093a000a
Shane Carvalho <shanec_@hotmail.com>
2015-10-27 17:57:35 +0530

    Cleaned up and parameterized the script
    Cleaned up, parameterized and commented out the script.

--- Azure/Update-SQLAzurePricingTier.ps1
+++ Azure/Update-SQLAzurePricingTier.ps1
@@ -1,20 +1,55 @@
+<#
+    .SYNOPSIS 
+		Updates the Azure SQL Database pricing tier.
+	.EXAMPLE
+		.\Update-SQLAzurePricingTier.ps1 -servername "myservername.database.windows.net" -databaseName "mydatabase" -username "myuser" -password "mypwd" -serviceObjectiveName "S0" -edition "Standard" -SleepDuration 10
+		Updates the Azure SQL Database pricing tier to Standard (S0)
+#>
+
+param(
+    [Parameter(Position=0,mandatory=$true)]
+    [string]$serverName,
+    [Parameter(Position=1,mandatory=$true)]
+    [string]$databaseName,
+    [Parameter(Position=2,mandatory=$true)]
+    [string]$username,
+    [Parameter(Position=3,mandatory=$true)]
+    [string]$password,
+    [Parameter(Position=4,mandatory=$true)]
+    [string]$serviceObjectiveName,
+    [Parameter(Position=5,mandatory=$true)]
+    [ValidateSet('Web', 'Business', 'Basic', 'Standard', 'Premium')]
+    [string]$edition,
+    [int] $sleepDuration = 0
+    )
+
+Write-Host "Initializing Azure SDK Module..." -ForegroundColor White
 
 # Ensure that the Azure SDK is imported
 Import-Module "C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\ServiceManagement\Azure\Azure.psd1"
 
-$user = "username"
-$pass = "password"
-$serverName = "myservername.database.windows.net"
-$databaseName = "mydatabasename"
+Write-Host "Processing credentials..." -ForegroundColor White
 
-$securePassword = ConvertTo-SecureString $pass -AsPlainText -Force
-# $credential = New-Object System.Management.Automation.PSCredential($user, $securepassword)
-$credential = Get-Credential
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($username, $securepassword)
+
+# $credential = Get-Credential
+
+Write-Host "Initializing server context..." -ForegroundColor White
 
 $serverContext = New-AzureSqlDatabaseServerContext -FullyQualifiedServerName $serverName -Credential $credential
 
-$db = Get-AzureSqlDatabase $serverContext –DatabaseName $databaseName 
+$db = Get-AzureSqlDatabase $serverContext -DatabaseName $databaseName 
+$so= Get-AzureSqlDatabaseServiceObjective $serverContext -ServiceObjectiveName $serviceObjectiveName
 
-$S1= Get-AzureSqlDatabaseServiceObjective $serverContext -ServiceObjectiveName "S1"
+Write-Host "Attempting to set the $databaseName datbase to $serviceObjectiveName ($edition)..." -ForegroundColor White
 
-Set-AzureSqlDatabase $serverContext –Database $db –ServiceObjective $S1 –Edition Standard
+Set-AzureSqlDatabase $serverContext -Database $db -ServiceObjective $so -Edition $edition -Force
+
+if ($sleepDuration -gt 0)
+{
+    Write-Host "Starting to sleep for $sleepDuration seconds..." -ForegroundColor White
+    Start-Sleep -Seconds $sleepDuration
+    Write-Host "Waking up!" -ForegroundColor White
+}
+
