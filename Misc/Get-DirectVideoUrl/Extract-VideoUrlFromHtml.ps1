ef816e516d249255bcb4b56b61e06d6f16aa6de7
Shane Carvalho <shanec_@hotmail.com>
2016-01-17 13:45:51 +0530

    Added Extract-VideoUrlFromHtml.ps1
    Added script to extract youtube urls from an html dump

---
+++ Misc/Get-DirectVideoUrl/Extract-VideoUrlFromHtml.ps1
@@ -0,0 +1,16 @@
+﻿param
+(
+    [string]$sourceFile,
+    [string]$destinationFile
+)
+
+# match all videos in the source file
+$results = Select-String -Path $sourceFile  -Pattern '(?<=\")/watch?(.*?)(?=\")'
+
+foreach($v in $results)
+{
+    $mergedResults += $v.Matches
+}
+
+# flatten results, remove duplicates, generate final url and write to file
+$mergedResults.Value | Sort-Object | Get-Unique | % { "http://www.youtube.com" + $_ } | Add-Content $destinationFile
